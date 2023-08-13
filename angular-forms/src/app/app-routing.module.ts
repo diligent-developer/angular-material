9bee01df9c73c72271bee616364c73d8f695c7de
diligentdeveloper <diligentdeveloper@icloud.com>
2023-08-13 23:02:05 +0800

    Introduction to Angular Reactive Forms

--- angular-forms/src/app/app-routing.module.ts
+++ angular-forms/src/app/app-routing.module.ts
@@ -1,46 +1,45 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
-import {CourseComponent} from './course/course.component';
-import {courseResolver} from './services/course.resolver';
-import {LoginComponent} from './login/login.component';
-import {CreateCourseComponent} from './create-course/create-course.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { courseResolver } from './services/course.resolver';
+import { LoginComponent } from './login/login.component';
+import { CreateCourseComponent } from './create-course/create-course.component';
+import { LoginReactiveComponent } from './login-reactive/login-reactive.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-
+    component: HomeComponent,
   },
   {
     path: 'about',
-    component: AboutComponent
+    component: AboutComponent,
   },
   {
     path: 'courses/:id',
     component: CourseComponent,
     resolve: {
-      course: courseResolver
-    }
+      course: courseResolver,
+    },
   },
   {
     path: 'add-new-course',
-    component: CreateCourseComponent
+    component: CreateCourseComponent,
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginReactiveComponent,
   },
   {
     path: '**',
-    redirectTo: '/'
-  }
+    redirectTo: '/',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {})],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
